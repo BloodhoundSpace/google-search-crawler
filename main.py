8575548136bc77f5f3dcb6c586031f299a42467c
huyiya <huyc26.dev@gmail.com>
2023-08-26 19:43:56 +0700

    fix: search results not found

--- main.py
+++ main.py
@@ -3,32 +3,35 @@ from ExcelWriter import ExcelWriter
 from logger import logger
 from configs import CRAWLER_CONFIGS
 
-RESULTS = []
-IS_RESULT_EMPTY = False
-
 if __name__ == '__main__':
   search_input = CRAWLER_CONFIGS['search_input']
   start_page = CRAWLER_CONFIGS['start_page']
   end_page = CRAWLER_CONFIGS['end_page']
+  
+  results = []
+  is_result_empty = False
   
   logger.info(f'🤖 Keyword: {search_input}')
   # Step 1: Crawl Data from Google Search
   crawler = GoogleSearchCrawler()
   while(
     start_page <= end_page
-    and not IS_RESULT_EMPTY
+    and not is_result_empty
   ):
     result = crawler.search(search_input, start_page)
     if (not result):
-      IS_RESULT_EMPTY = True
+      is_result_empty = True
     else:
-      RESULTS.extend(result)
+      results.extend(result)
       start_page += 1
 
   # Step 2: Save results data to Excel with filename = search string
   filename = search_input.lower().replace(' ', '-')
-  excel_writer = ExcelWriter(filename)
-  excel_writer.save_to_excel(RESULTS)
+  if (not results):
+    logger.info('🚬 No results found')
+  else:
+    excel_writer = ExcelWriter(filename)
+    excel_writer.save_to_excel(results)
 
-  logger.info('🎉 Done')
+    logger.info('🎉 Done')
   
