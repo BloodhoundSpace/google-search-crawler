c9195bfef57cca9029ac7d0a87cb23968392bbc8
huyiya <huyc26.dev@gmail.com>
2023-08-29 01:07:42 +0700

    feat: extract email of the site

--- HtmlExtractor.py
+++ HtmlExtractor.py
@@ -1,11 +1,30 @@
 import re
+import requests
+from bs4 import BeautifulSoup
+from logger import logger
 
 class HtmlExtractor():
-  def __init__(self) -> None:
-    pass
+  def __init__(self, url: str) -> None:
+    super().__init__()
+
+    self.url = url
+    self.email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+  def _get_soup(self):
+    try:
+      res = requests.get(self.url, timeout=5)
+      soup = BeautifulSoup(res.text, 'html.parser')
+      return soup
+    except Exception as e:
+      logger.error('Error', e)
+      return ''
 
   # Ref: https://stackoverflow.com/a/24618186
-  def extract_text(self, soup) -> str:
+  def _extract_text_from_soup(self) -> str:
+    soup = self._get_soup()
+    if (not soup):
+      return ''
+    
     # kill all script and style elements
     for script in soup(['script', 'style']):
       script.extract() # rip it out
@@ -22,11 +41,15 @@ class HtmlExtractor():
 
     return text
   
-  def get_email(self, text: str) -> str:
-    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-    email_addresses = re.findall(email_pattern, text)
+  def get_email(self) -> str:
+    html_text = self._extract_text_from_soup()
+    
+    email_addresses = re.findall(self.email_pattern, html_text)
     # Remove duplicated email
     email_addresses = list(set(email_addresses))
 
+    if (len(email_addresses) == 0):
+      return ''
+    
     return email_addresses
   
